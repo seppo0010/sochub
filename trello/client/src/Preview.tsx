8a901ec1b3bb6146c202dc7e0675740ef5027d66
Sebastian Waisbrot <seppo0010@gmail.com>
2020-12-30 13:07:38 -0300

    Preview switcher

--- trello/client/src/Preview.tsx
+++ trello/client/src/Preview.tsx
@@ -3,7 +3,11 @@ import { getCode } from './Input'
 import { Preview as TwitterPreview } from './Output/Twitter'
 import { Preview as MediumPreview } from './Output/Medium'
 
+const PREVIEW_TWITTER = 'twitter'
+const PREVIEW_MEDIUM = 'medium'
+
 export default function Preview() {
+    const [displayPreview, setDisplayPreview] = useState(PREVIEW_TWITTER)
     const [loaded, setLoaded] = useState(false)
     const [code, setCode] = useState('')
     useState(async () => {
@@ -12,7 +16,13 @@ export default function Preview() {
     })
     return <div style={{overflow: 'auto', maxHeight: '1500px'}}>
         {!loaded && <b>loading...</b>}
-        {loaded && <TwitterPreview code={code} />}
-        {loaded && <MediumPreview code={code} />}
+        {loaded && <div>
+            <ul style={{display: 'flex', margin: 20,}}>
+                <li><button onClick={() => setDisplayPreview(PREVIEW_TWITTER)}>Twitter</button></li>
+                <li><button onClick={() => setDisplayPreview(PREVIEW_MEDIUM)}>Medium</button></li>
+            </ul>
+            {displayPreview == PREVIEW_TWITTER && <TwitterPreview code={code} />}
+            {displayPreview == PREVIEW_MEDIUM && <MediumPreview code={code} />}
+        </div>}
     </div>
 }
