a080d0797f94de34d1a0d7f0b638c0ade1d0cbe6
Sebastian Waisbrot <seppo0010@gmail.com>
2020-12-31 15:21:20 -0300

    Fix path

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(express.static('public'));
 if (false) {
     app.use('/', proxy('localhost:3001'));
 } else {
-    app.use('/trello', express.static('trello/client/build'));
+    app.use('/trello', express.static('../trello/client/build'));
     app.get('/trello/*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'trello/client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../trello/client/build', 'index.html'));
     });
 }
 
